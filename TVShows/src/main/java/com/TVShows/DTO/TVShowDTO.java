fef4e8e2acb90fb728298444da6664de29f945cf
Taras Peles <pelestaras@gmail.com>
2023-11-07 13:19:45 +0200

    Search fix

--- TVShows/src/main/java/com/TVShows/DTO/TVShowDTO.java
+++ TVShows/src/main/java/com/TVShows/DTO/TVShowDTO.java
@@ -2,6 +2,8 @@ package com.TVShows.DTO;
 
 import com.TVShows.domain.Genre;
 import com.TVShows.enums.ShowStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.squareup.moshi.Json;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,18 +11,21 @@ import java.util.List;
 
 @Getter @Setter
 public class TVShowDTO {
-
     private Long id;
     private String name;
     private Integer episodesNumber;
     private String releaseDate;
-    private List<Genre> genre;
     private String directors;
     private String description;
     private String imageUrl;
     private String trailerUrl;
     private String nextEpisode;
-    private ShowStatus status;
     private String episodeDuration;
     private String actors;
+
+    @JsonIgnore
+    private List<Genre> genre;
+
+    @JsonIgnore
+    private ShowStatus status;
 }
