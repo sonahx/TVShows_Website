781e82dc720e2e62adf1081d10f1db9558b79b15
peles-taras <pelestaras@gmail.com>
2024-06-03 22:28:12 +0300

    Added dotenv

---
+++ TVShows/src/main/java/com/TVShows/config/DotenvConfig.java
@@ -0,0 +1,18 @@
+package com.TVShows.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DotenvConfig {
+
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.configure()
+                .directory("/app")
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+    }
+}
