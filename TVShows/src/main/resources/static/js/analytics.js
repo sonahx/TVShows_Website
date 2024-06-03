82402de3ba573e7e2618488cc3e15fdcb7da6993
peles-taras <pelestaras@gmail.com>
2024-06-03 21:34:19 +0300

    Added traffic analytics

---
+++ TVShows/src/main/resources/static/js/analytics.js
@@ -0,0 +1,15 @@
+(function() {
+    var script = document.createElement('script');
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-CRQ7L9FJH6";
+    script.async = true;
+    document.head.appendChild(script);
+  
+    script.onload = function() {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+  
+      gtag('config', 'G-CRQ7L9FJH6');
+    };
+  })();
+  
